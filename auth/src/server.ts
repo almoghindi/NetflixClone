e9e1dab87b5f8e71c57813700b660030785ad634
Elior Tansoura <eliortan177@gmail.com>
2024-08-05 16:33:06 +0300

    cors configurations added.

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,12 +1,15 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import { corsConfiguration } from './configurations/cors';
+import cors from 'cors';
 
-//For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors(corsConfiguration()));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
