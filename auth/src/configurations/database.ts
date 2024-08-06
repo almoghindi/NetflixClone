b33071108417fe491d521dc657352766eb9dc48e
Elior Tansoura <eliortan177@gmail.com>
2024-08-06 14:14:31 +0300

    DB configurations update

--- auth/src/configurations/database.ts
+++ auth/src/configurations/database.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
-export const DBConnect = () => {
-    mongoose.connect(process.env.MONGODB_URI!).then(() => {
-        console.log('MongoDB Connected');
-      }).catch((err) => {
-        console.log(err.message);
-      });
-}      
+export const DBConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
