af3422e1545ba5808ff587e89ae723d7731a2174
almoghindi <almoghinde@gmail.com>
2024-08-06 15:40:17 +0300

    add infra of docker

--- auth/src/configurations/database.ts
+++ auth/src/configurations/database.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 export const DBConnect = () => {
-    mongoose.connect(process.env.MONGODB_URI!).then(() => {
-        console.log('MongoDB Connected');
-      }).catch((err) => {
-        console.log(err.message);
-      });
-}      
+  mongoose
+    .connect(process.env.MONGO_URI!)
+    .then(() => {
+      console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
