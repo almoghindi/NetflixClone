c040f4d3f14ee477ebcfb45a5ccceaeedd104889
Alon Ben Zaken <alonbnzn@gmail.com>
2024-08-08 15:13:26 +0300

    change subscription from account to auth 2

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,23 +1,32 @@
 import { ObjectId } from "mongodb";
 import mongoose from "mongoose";
 
+enum SubscriptionType {
+  BASIC = "BASIC",
+  STANDART = "STANDART",
+  PREMIUM = "PREMIUM",
+}
 const UserSchema = new mongoose.Schema({
-    id: {
-        type: ObjectId,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type:String,
-        required: true,
-    },
-    token: {
-        type: String,
-        required: true,
-    },
+  id: {
+    type: ObjectId,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  subscription: {
+    type: SubscriptionType,
+    required: true,
+  },
 });
 
 export default mongoose.model("User", UserSchema);
