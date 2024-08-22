8904e418e69c76aa830d8120ca127b7a9660c05a
Elior Tansoura <eliortan177@gmail.com>
2024-08-22 18:29:39 +0300

    tests for the Auth service controllers. all passed.

--- auth/jest.config.js
+++ auth/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
 };
