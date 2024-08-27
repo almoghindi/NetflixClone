414beafc705ac59600d9025865a13b22995d1de3
Alon Ben Zaken <alonbnzn@gmail.com>
2024-08-27 17:36:22 +0300

    payment user update

--- payment/src/controllers/payment-controller.ts
+++ payment/src/controllers/payment-controller.ts
@@ -3,12 +3,12 @@ import { plans, stripe } from "../stripe";
 import Stripe from "stripe";
 
 export const createSubscription = async (req: Request, res: Response) => {
-  const { plan, userId } = req.body;
+  const { plan, user } = req.body;
 
   const customer = await stripe.customers.create({
-    name: userId,
+    name: user.email,
     metadata: {
-      user_id: userId,
+      user_id: user.id,
     },
   });
   const priceId = plans.find((p) => p.type === plan)!.priceId;
